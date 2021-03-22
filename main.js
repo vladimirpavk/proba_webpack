df534ae5da5cdf838a77ae8da0da94bd30c1fa20
Vladimir Pavković <vladimirpavk@gmail.com>
2021-03-22 14:49:36 +0100

    +

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import * as Main from './src/main.js';
-
+/* import * as Main from './src/main.js';
+ */
 /* console.log(Main.saberi_sa_5(10)); */
+import { combAll } from './bundle.js';
+console.log(combAll(['a', 'c', 'd'], 'f'));
+
 let numberElement = document.getElementById('my_number');
 numberElement.innerText = Main.saberi_sa_5(12);
 
--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ import * as Utils from './utils.js';
 export const saberi_sa_5 = (broj)=>{
     return Utils.broj5 + broj;
 }
+
+export const PavleBroj = 7;
