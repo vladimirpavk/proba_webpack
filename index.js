1480ccf6a74809cfec23c6eca9a21b6b415aa008
Vladimir Pavković <vladimirpavk@gmail.com>
2021-04-06 12:22:43 +0200

    +

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 import * as ItemPicker from './src/components/itemPicker/itemPicker.js';
 import * as Item from './src/components/Item/item.js';
 
-import { elementsArray } from './src/storage/storage.js';
+//import { elementsArray } from './src/storage/storage.js';
+//import { storage } from './src/storage/storage.js';
+import * as Storage from './src/storage/storageClass.js';
 
 const rootElement = document.getElementById('root');
+let arrayElements = document.createElement('div');
+
+let storage = document.createElement('stor-age');
+storage.addEventListener('storage-changed', (eventData)=>{    
+    console.log('Storage changed event...', eventData.detail);
+});
+storage.addEventListener('item-added', (eventData)=>{
+    console.log('Item added...', eventData.detail);
+});
+
+let itemPicker = document.createElement('item-picker');
+itemPicker.addEventListener('itempicked', (eventData)=>{    
+   
+    storage.addItem(eventData.detail);
+
+    let newItem = document.createElement('it-em');
+    newItem.setAttribute('value', eventData.detail);
+    newItem.addEventListener('item-removed', (eventData)=>{
+        console.log(eventData);
+    });
+    arrayElements.appendChild(newItem);
+});
+
+rootElement.appendChild(itemPicker);
+rootElement.appendChild(arrayElements); 
+
+/* const rootElement = document.getElementById('root');
 
 let arrayElements = document.createElement('div');
 
@@ -21,4 +50,4 @@ itemPicker.addEventListener('itempicked', (eventData)=>{
 });
 
 rootElement.appendChild(itemPicker);
-rootElement.appendChild(arrayElements);
+rootElement.appendChild(arrayElements); */
--- src/storage/storageClass.js
+++ src/storage/storageClass.js
@@ -1,12 +1,16 @@
-export class StorageClass{
-    
+class StorageClass extends HTMLElement{
+
+    #storage = [];
+
     constructor(){
-        this.#storage = [];
+        super();
     }
 
     addItem = (item)=>{
         this.#storage.push(item);
-        this.dispatchEvent(new CustomEvent('item-added'), {detail: item});
-        this.dispatchEvent(new CustomEvent('storage-changed'), {detail:this.#storage});
+        this.dispatchEvent(new CustomEvent('item-added', {detail: item}));
+        this.dispatchEvent(new CustomEvent('storage-changed', {detail:this.#storage}));
     }
 }
+
+window.customElements.define('stor-age', StorageClass);
